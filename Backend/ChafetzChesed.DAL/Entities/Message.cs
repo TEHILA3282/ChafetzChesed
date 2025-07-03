4d3f65d6be4426b8668067f6388374e55bf25e1c
Elisheva Tzemach <elisheva2005z@gmail.com>
2025-07-03 14:40:58 +0300

    שומרים קבצי הודעות חדשים לפני משיכת עדכונים

---
+++ Backend/ChafetzChesed.DAL/Entities/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChafetzChesed.DAL.Entities
+{
+    public class Message
+    {
+        public int ID { get; set; }
+        public string ClientID { get; set; } = null!; // קישור ל-Registration
+        public string MessageType { get; set; } = null!; // 'מערכת' או 'גביה'
+        public string MessageText { get; set; } = null!;
+        public DateTime DateSent { get; set; }
+        public bool IsRead { get; set; }
+    }
+}
