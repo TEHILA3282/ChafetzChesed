dcb1659f0cd69633e7bc9d682f13372c02cf8a07
TEHILA3282 <TE3282@GMAIL.COM>
2025-08-19 17:53:59 +0300

    עדכון app.ts ו-main.ts לשינוי title ולוגו לפי גמ

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,19 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
-import { HeaderComponent } from './components/header/header';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { RouterOutlet } from '@angular/router';   
+import { InstitutionService } from './services/institution.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    HttpClientModule,
-    HeaderComponent
-  ],
-  templateUrl: './app.html',
-  styleUrls: ['./app.scss']
+  imports: [RouterOutlet],  
+  template: '<router-outlet></router-outlet>'
 })
-export class App {
-  protected title = 'frontend';
+export class AppComponent implements OnInit {
+
+  constructor(
+    private titleService: Title,
+    private institutionService: InstitutionService
+  ) {}
+
+  ngOnInit(): void {
+    const inst = this.institutionService.getInstitution();
+
+    this.titleService.setTitle(inst.name);
+
+    const link: HTMLLinkElement =
+      document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'icon';
+    link.href = inst.logo;
+    document.getElementsByTagName('head')[0].appendChild(link);
+  }
 }
